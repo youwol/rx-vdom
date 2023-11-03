ce668abd51ab350e9478346e5b2473aaa96c16f8
youwol-guillaume <greinisch@youwol.com>
2023-11-03 17:32:37 +0100

    🔧 `yw_pipeline.py` => add `assets` folder in `PublishConfig`

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -1,6 +1,10 @@
-from youwol.app.environment import YouwolEnvironment, IPipelineFactory
-from youwol.app.environment.models_project import JsBundle, Link
-from youwol.pipelines.pipeline_typescript_weback_npm import PipelineConfig, pipeline
+from youwol.app.environment import YouwolEnvironment
+from youwol.app.routers.projects import IPipelineFactory, JsBundle, Link, Pipeline
+from youwol.pipelines.pipeline_typescript_weback_npm import (
+    pipeline,
+    PipelineConfig,
+    PublishConfig,
+)
 from youwol.utils.context import Context
 
 
@@ -8,7 +12,7 @@ class PipelineFactory(IPipelineFactory):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    async def get(self, env: YouwolEnvironment, context: Context):
+    async def get(self, env: YouwolEnvironment, context: Context) -> Pipeline:
         config = PipelineConfig(
             target=JsBundle(
                 links=[
@@ -16,6 +20,7 @@ class PipelineFactory(IPipelineFactory):
                     Link(name="coverage", url="coverage/lcov-report/index.html"),
                     Link(name="bundle-analysis", url="dist/bundle-analysis.html"),
                 ]
-            )
+            ),
+            publishConfig=PublishConfig(packagedFolders=["./assets"]),
         )
         return await pipeline(config, context)
