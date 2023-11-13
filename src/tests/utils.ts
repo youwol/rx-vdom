bf106a47716fd4c7b547aec1df11631f535158e0
youwol-guillaume <greinisch@youwol.com>
2023-11-13 11:49:49 +0100

    ✅ test-utils => improve type annotations

--- src/tests/utils.ts
+++ src/tests/utils.ts
@@ -1,5 +1,5 @@
-export const orderedChildren = (elem: HTMLElement) =>
+export const orderedChildren = (elem: HTMLElement): HTMLElement[] =>
     [...elem.children].sort(
         (a: HTMLElement, b: HTMLElement) =>
             parseInt(a.style.order) - parseInt(b.style.order),
-    )
+    ) as unknown as HTMLElement[]
