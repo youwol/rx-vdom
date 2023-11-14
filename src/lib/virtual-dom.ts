173041bd67bde7f0312c7982c27afca3e113b1d9
youwol-guillaume <greinisch@youwol.com>
2023-11-14 10:51:45 +0100

    📝 inlined snippet: use `@youwol/webpm-client`

--- src/lib/virtual-dom.ts
+++ src/lib/virtual-dom.ts
@@ -31,14 +31,13 @@ import { factory, SupportedTags } from './factory'
  * <script>
  *  const src = `<!--<!DOCTYPE html>
  * <html lang="en">
- *   <head><script src="https://webpm.org/^2.1.2/cdn-client.js"></script></head>
+ *   <head><script src="https://webpm.org/^2.2.0/webpm-client.js"></script></head>
  *
  *   <body id="content"></body>
  *
  *   <script type="module">
- *      const cdnClient = window['@youwol/cdn-client']
- *      const { rxDom, rxjs } = await cdnClient.install({
- *          modules: ['@youwol/rx-vdom as rxDom', 'rxjs#^7.5.6'],
+ *      const { rxDom, rxjs } = await webpm.install({
+ *          modules: ['@youwol/rx-vdom as rxDom', 'rxjs#^7.5.6' as rxjs],
  *          displayLoadingScreen: true
  *      });
  *      const vDOM = {
