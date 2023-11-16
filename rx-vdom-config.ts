4ffbd18695aff25a3108d43bd7d6018267300f10
youwol-guillaume <greinisch@youwol.com>
2023-11-16 09:46:20 +0100

    ♻️ set up the project to test `WithFluxView`

--- rx-vdom-config.ts
+++ rx-vdom-config.ts
@@ -2,7 +2,7 @@ type AllTags = keyof HTMLElementTagNameMap
 export type Configuration = {
     TypeCheck: 'strict'
     SupportedHTMLTags: 'Prod' extends 'Prod' ? AllTags : DevTags
-    WithFluxView: false
+    WithFluxView: true
 }
 
 type DevTags =
