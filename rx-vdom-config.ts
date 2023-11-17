050b45c85730fe10a52e83de6ad6b8f0935b4cce
youwol-guillaume <greinisch@youwol.com>
2023-11-17 17:23:19 +0100

    🔧 `rx-vdom-config.ts` => `DevTags` pick only the used ones

--- rx-vdom-config.ts
+++ rx-vdom-config.ts
@@ -5,16 +5,4 @@ export type Configuration = {
     WithFluxView: true
 }
 
-type DevTags =
-    | 'a'
-    | 'b'
-    | 'blockquote'
-    | 'ul'
-    | 'li'
-    | 'div'
-    | 'h1'
-    | 'h2'
-    | 'h3'
-    | 'h4'
-    | 'h5'
-    | 'h6'
+type DevTags = 'a' | 'b' | 'blockquote' | 'div'
